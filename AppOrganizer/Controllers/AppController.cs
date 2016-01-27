f115bccc23bf0fb46744ee103e270253a73f2df9
Pito289 <Juan.M.Davila.Jr@hotmail.com>
2016-01-27 17:25:32 -0600

    Initial commit of user authorization for App Organizer.

---
+++ AppOrganizer/Controllers/AppController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppOrganizer.Controllers
+{
+    [Authorize(Roles="admin, developer")]
+    public class AppController : Controller
+    {
+        // GET: App
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ContentResult EnclosedApp()
+        {
+            return Content("This is an enclosed App.");
+        }
+
+        public ContentResult ViewableApp()
+        {
+            return Content("This is a viewable App.");
+        }
+    }
+}
---
+++ AppOrganizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppOrganizer.Startup))]
+namespace AppOrganizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
